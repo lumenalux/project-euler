1ea051ac06f0083b5075d2ee047d023324025b4c
Stefan Martyniuk <stefanmartinuk@gmail.com>
2024-01-18 20:36:40 +0200

    Add problem 22

---
+++ problem_022_names_scores.py
@@ -0,0 +1,28 @@
+"""
+Using resources/0022_names.txt, a 46K text file containing over five-thousand
+first names, begin by sorting it into alphabetical order. Then working out the
+alphabetical value for each name, multiply this value by its alphabetical value
+to obtain a name score.
+
+For example, when the list is sorted into alphabetical order, COLIN, which is
+worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would
+obtain a score of 938 x 53 = 49714.
+
+What is the total of all the name scores in the file?
+
+Answer: 871198282
+link: https://projecteuler.net/problem=22
+"""
+from heapq import heappush, heappop
+from itertools import chain
+
+
+def solution(file_name):
+    with open(file_name) as f:
+        return sum((i + 1) * sum(ord(c) - 64 for c in name.strip('"'))
+                   for i, name in enumerate(sorted(f.read().split(','))))
+
+
+# test
+if __name__ == '__main__':
+    print(solution('resources/0022_names.txt')) # 871198282
