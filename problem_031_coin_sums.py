11ef055a07f86b0ec9a6e62b73c415065e678c5e
Stefan Martyniuk <stefanmartinuk@gmail.com>
2024-01-20 13:03:28 +0200

    Add problem 031: Coin Sums

---
+++ problem_031_coin_sums.py
@@ -0,0 +1,34 @@
+"""
+In the United Kingdom the currency is made up of pound (£) and pence (p). There
+are eight coins in general circulation:
+
+    1p, 2p, 5p, 10p, 20p, 50p, £1 (100p), and £2 (200p).
+
+It is possible to make £2 in the following way:
+
+    1x£1 + 1x50p + 2x20p + 1x5p + 1x2p + 3x1p
+
+How many different ways can £2 be made using any number of coins?
+
+link: https://projecteuler.net/problem=31
+
+Solution:
+
+Nothing special, just a classic dynamic programming problem.
+
+If you are not familiar with dynamic programming, you can read about it in
+chapter 9 of the book "Grokking Algorithms" by Aditya Bhargava.
+link: https://livebook.manning.com/book/grokking-algorithms/chapter-9/1
+"""
+def solution(N):
+    coins = [1, 2, 5, 10, 20, 50, 100, 200]
+    dp = [0] * (N + 1)
+    dp[0] = 1
+    for coin in coins:
+        for i in range(coin, N + 1):
+            dp[i] += dp[i - coin]
+    return dp[N]
+
+
+if __name__ == '__main__':
+    print(solution(200)) # 73682
