ead265144a05a320de324ecb548204da6cb9f432
Stefan Martyniuk <stefanmartinuk@gmail.com>
2024-01-29 10:20:11 +0200

    Refactor code and add problem description

--- problem_062_cubic_permutations.py
+++ problem_062_cubic_permutations.py
@@ -1,10 +1,14 @@
 """
+The cube, 41063625 (345^3), can be permuted to produce two other cubes:
+56623104 (384^3) and 66430125 (405^3). In fact, 41063625 is the smallest cube
+which has exactly three permutations of its digits which are also cube.
+
+Find the smallest cube for which exactly five permutations of its digits are
+cube.
+
+link: https://projecteuler.net/problem=62
 """
-from itertools import count, permutations
-
-
-def is_cube(n):
-    return round(n ** (1/3)) ** 3 == n
+from itertools import count
 
 
 def has_exactly_n_permutations(n, cube):
