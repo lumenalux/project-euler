fe84f397fe2612eceb15c4b6b3f2f029fa7e70ac
Stefan Martyniuk <stefanmartinuk@gmail.com>
2024-01-30 14:01:32 +0200

    Add link to problem statement

--- problem_069_totient_maximum.py
+++ problem_069_totient_maximum.py
@@ -19,6 +19,8 @@ It can be seen that n=6 produces a maximum n/φ(n) for n ≤ 10.
 
 Find the value of n ≤ 1,000,000 for which n/φ(n) is a maximum.
 
+link: https://projecteuler.net/problem=69
+
 Solution:
 
 The totient function can be found using the formula:
