a1ebff3a4640304bf64789215be429079a94ad2a
Stefan Martyniuk <stefanmartinuk@gmail.com>
2024-02-01 19:31:14 +0200

    Update link in passcode derivation file

--- problem_079_passcode_derivation.py
+++ problem_079_passcode_derivation.py
@@ -11,7 +11,7 @@ Given that the three characters are always asked for in order, analyse the
 file so as to determine the shortest possible secret passcode of unknown
 length.
 
-link: projecteuler.net/problem=79
+link: https://projecteuler.net/problem=79
 
 Solution:
 
