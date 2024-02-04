0860597cdd9730ea3654d9f63f8429de59d89043
Stefan Martyniuk <stefanmartinuk@gmail.com>
2024-02-04 14:44:53 +0200

    Refactor solution and dp_solution functions in problem_015_lattice_paths.py

--- problem_015_lattice_paths.py
+++ problem_015_lattice_paths.py
@@ -12,19 +12,19 @@ import math
 
 # O(n^2) time | O(n^2) space
 def dp_solution(N):
-  grid = [[1] * (N + 1)] + [[1] + [0] * N for _ in range(N)]
-  for i in range(1, N + 1):
-    for j in range(1, N + 1):
-      grid[i][j] = grid[i - 1][j] + grid[i][j - 1]
-  return grid[-1][-1]
+    grid = [[1] * (N + 1)] + [[1] + [0] * N for _ in range(N)]
+    for i in range(1, N + 1):
+        for j in range(1, N + 1):
+            grid[i][j] = grid[i - 1][j] + grid[i][j - 1]
+    return grid[-1][-1]
 
 
 # O(n) time | O(1) space
 def solution(N):
-  return math.comb(2 * N, N)
+    return math.comb(2 * N, N)
 
 
 # test
 if __name__ == '__main__':
-  print(solution(20)) # 137846528820
-  print(dp_solution(20)) # 137846528820
+    print(solution(20))     # 137846528820
+    print(dp_solution(20))  # 137846528820
