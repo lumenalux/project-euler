6e13459b9d7748f5978752c2939ee6ae839cd232
Stefan Martyniuk <stefanmartinuk@gmail.com>
2024-02-04 13:38:47 +0200

    Fix formatting issues on problem 006

--- problem_006_sum_square_difference.py
+++ problem_006_sum_square_difference.py
@@ -43,7 +43,7 @@ def brute_force_solution(n):
 
 # test
 if __name__ == '__main__':
-    print(brute_force_solution(10)) # 2640
-    print(solution(10)) # 2640
-    print(brute_force_solution(100)) # 25164150
-    print(solution(100)) # 25164150
+    print(brute_force_solution(10))   # 2640
+    print(solution(10))               # 2640
+    print(brute_force_solution(100))  # 25164150
+    print(solution(100))              # 25164150
