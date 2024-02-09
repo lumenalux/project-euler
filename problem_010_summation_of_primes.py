eddbf0207cc279c53b14b58648c3d7fdf90102d9
Stefan Martyniuk <stefanmartinuk@gmail.com>
2024-02-09 15:03:41 +0200

    Refactor sieve_of_eratosthenes function and update test cases

--- problem_010_summation_of_primes.py
+++ problem_010_summation_of_primes.py
@@ -8,6 +8,8 @@ link: https://projecteuler.net/problem=10
 
 # For more details about the algorithm:
 # https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+
+
 def sieve_of_eratosthenes(n):
     """Sieve of Eratosthenes algorithm to find all primes less than n."""
     sieve = [True] * ((n + 1) // 2)
@@ -16,7 +18,8 @@ def sieve_of_eratosthenes(n):
             start, end, step = i*i // 2, n // 2, i
             sieve[start:end:step] = [False] * ((end - start - 1) // step + 1)
 
-    return [] if n < 3 else [2, *(2*i + 1 for i in range(1, n // 2) if sieve[i])]
+    return [] if n < 3 else [2, *(2*i + 1
+                                  for i in range(1, n // 2) if sieve[i])]
 
 
 # O(n*log(log(n))) - Sieve of Eratosthenes
@@ -26,5 +29,5 @@ def solution(n):
 
 # test
 if __name__ == '__main__':
-    print(solution(10)) # 17
-    print(solution(2_000_000)) # 142913828922
+    print(solution(10))  # 17
+    print(solution(2_000_000))  # 142913828922
