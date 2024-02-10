2a4e469af2560d1852468a0a1b509499f0ba9def
Stefan Martyniuk <stefanmartinuk@gmail.com>
2024-02-10 13:22:34 +0200

    Update factorial multiplication operator to 'x'

--- problem_020_factorial_digit_sum.py
+++ problem_020_factorial_digit_sum.py
@@ -1,7 +1,7 @@
 """
-n! means n × (n − 1) × ... × 3 × 2 × 1.
+n! means n x (n - 1) x ... x 3 x 2 x 1.
 
-For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800, and the sum
+For example, 10! = 10 x 9 x ... x 3 x 2 x 1 = 3628800, and the sum
 of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 
 Find the sum of the digits in the number 100!
@@ -25,10 +25,12 @@ calculate the odd product in O(n) time.
 """
 import math
 
+
 def generate_factorial_steps(n):
-        if n > 1:
-            yield from generate_factorial_steps(n - 1 if n % 2 else n // 2)
-            yield n
+    if n > 1:
+        yield from generate_factorial_steps(n - 1 if n % 2 else n // 2)
+        yield n
+
 
 def fast_factorial(n):
     result = 1
@@ -52,5 +54,6 @@ def fast_factorial(n):
 def solution(n):
     return sum(map(int, str(fast_factorial(n))))
 
+
 if __name__ == '__main__':
-    print(solution(100)) # 648
+    print(solution(100))  # 648
