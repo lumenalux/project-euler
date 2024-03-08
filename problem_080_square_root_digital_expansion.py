a36971c194647c18076df48f43ac058695958339
Stefan Martyniuk <stefanmartinuk@gmail.com>
2024-03-08 15:54:53 +0200

    Refactor square root digital expansion functions

--- problem_080_square_root_digital_expansion.py
+++ problem_080_square_root_digital_expansion.py
@@ -18,14 +18,14 @@ import math
 from decimal import Decimal, getcontext
 
 
-def decimal_module_solution(N, K):
+def decimal_module_solution(N: int, K: int) -> int:
     getcontext().prec = K + 10
     return sum(sum(map(int, str(Decimal(n).sqrt()).replace('.', '')[:K]))
                for n in range(1, N + 1)
                if math.isqrt(n) ** 2 != n)
 
 
-def get_sum_of_nth_digits_of_square_root(number, k):
+def get_sum_of_nth_digits_of_square_root(number: int, k: int) -> int:
     high, low, limit = 5*number, 5, 10**(k + 10)
 
     while low < limit:
@@ -39,7 +39,7 @@ def get_sum_of_nth_digits_of_square_root(number, k):
     return sum(map(int, str(low)[:k]))
 
 
-def solution(N, K):
+def solution(N: int, K: int) -> int:
     return sum(get_sum_of_nth_digits_of_square_root(n, K)
                for n in range(1, N + 1)
                if math.isqrt(n) ** 2 != n)
